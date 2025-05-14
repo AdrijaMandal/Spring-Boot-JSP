725d8641fd6252b4ca78aea8590e658e28a10e02
AdrijaMandal <adrijamandal0@gmail.com>
2025-05-14 10:42:30 +0530

    Create OrderEntity.java

---
+++ OrderEntity.java
@@ -0,0 +1,48 @@
+package com.bca.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String product;
+    private String customer;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
